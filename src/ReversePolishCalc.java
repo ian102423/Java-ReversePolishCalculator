f05549b83738da91491ea40eb455b7d9116bfb58
Ian Lee <ian10242012@gmail.com>
2017-07-24 10:18:42 -0500

    Final Change - Working Code

--- src/ReversePolishCalc.java
+++ src/ReversePolishCalc.java
@@ -1,40 +1,54 @@
+import java.util.Arrays;
 
 public class ReversePolishCalc {
 
-    // You'll need a variable here to keep track of the top of the stack
-
-    // The array of the input string split up
     private String[] tokens;
-
-    // The stack
     private String[] stack;
+    private int top = -1;
 
     public double calculate(String input) {
 
-        // 1. Use the String split method to split the string into tokens at the commas
-        tokens = ...
+        tokens = input.split(",");
+        stack = new String[tokens.length];
 
-        // 2. Allocate a stack as big as the number of tokens
-        stack = ...
-
-        // 3. write the algorithm
-        for(int i = 0; i < tokens.length; ++i) {
-            // calls to push() and pop() and do the math here
+        for (int i = 0; i < tokens.length; ++i) {
+            String token = tokens[i];
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                double firstOp = pop();
+                double secondOp = pop();
+                if (token.equals("+")) {
+                    push(firstOp + secondOp);
+                } else if (token.equals("-")) {
+                    push(firstOp - secondOp);
+                } else if (token.equals("*")) {
+                    push(firstOp * secondOp);
+                } else if (token.equals("/")) {
+                    push(firstOp / secondOp);
+                }
+            } else {
+                push(token);
+            }
         }
-
-        // 4. return the result
         return pop();
     }
 
     private void push(String number) {
-        // push on the stack
+        top++;
+        stack[top] = number;
     }
 
     private void push(double d) {
-        // change the double to a string and then push it on the stack
+        String pushString = String.valueOf(d);
+        push(pushString);
     }
 
     private double pop() {
-        // remove the string from the top of the stack and convert it to a double and return it
+        double popString = Double.parseDouble(stack[top]);
+        stack[top] = null;
+        if (top != 0) {
+            top--;
+        }
+        return popString;
     }
 }
+
